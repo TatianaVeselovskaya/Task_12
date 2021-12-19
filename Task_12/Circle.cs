fae1678e8f64e9b681b72c90b62432d26b72536d
Татьяна <veselovskayt@mail.ru>
2021-12-19 18:11:00 +0700

    Добавьте файлы проекта.

---
+++ Task_12/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+    class Circle
+    {
+        static public double Getlength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static public Boolean GetPrinad(double x, double y, double x0, double y0, double r)
+        {
+            if ((x - x0) * (x - x0) + (y - y0) * (y - y0) <= r * r)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Task_12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус окружности r=");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты центра окружности (х0;у0)");
+            Console.Write("Введите координаты центра окружности x0= ");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координаты центра окружности y0= ");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки (х;у)");
+            Console.Write("Введите x= ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y= ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double length = Circle.Getlength(r);
+            double Square = Circle.GetSquare(r);
+            Boolean Prinad = Circle.GetPrinad(x, y, x0, y0, r);
+
+            Console.WriteLine($"Длина окружности = {length} \nПлощадь круга = {Square} \nПринаддежность точки кругу - {Prinad}");
+            Console.ReadKey();
+        }
+    }
+}
